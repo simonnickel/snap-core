b4f4b50bab6e352ad6a10badc1bdae819c2e82ee
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 11:03:34 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "snap-core",
+	platforms: [
+		.iOS(.v17), .macOS(.v14)
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,9 +18,14 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "snap-core"),
+            name: "snap-core",
+			swiftSettings: [
+				.enableExperimentalFeature("StrictConcurrency")
+			]
+		),
         .testTarget(
             name: "snap-coreTests",
-            dependencies: ["snap-core"]),
+            dependencies: ["snap-core"]
+		),
     ]
 )
