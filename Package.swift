c885dbd2f8a35efe1ad142826ec81cf7c6a10075
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 10:58:34 +0200

    Create package with Xcode 15.3.0

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "snap-core",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "snap-core",
+            targets: ["snap-core"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "snap-core"),
+        .testTarget(
+            name: "snap-coreTests",
+            dependencies: ["snap-core"]),
+    ]
+)
