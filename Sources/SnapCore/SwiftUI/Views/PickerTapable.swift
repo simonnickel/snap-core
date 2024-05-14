0efa53a390859af07b491be4045257f9a9da77e1
Simon Nickel <simonnickel@googlemail.com>
2024-05-14 10:02:20 +0200

    Update documentation

--- Sources/SnapCore/SwiftUI/Views/PickerTapable.swift
+++ Sources/SnapCore/SwiftUI/Views/PickerTapable.swift
@@ -7,13 +7,13 @@
 
 import SwiftUI
 
-/// Wrapper for Picker to include PickerStyle and fix some issues.
+/// Wrapper of Picker to include PickerStyle and fix some issues. See SnapTheme.ThemePickerListRow for a version with styling.
 ///
 ///	- FB13208225: setting the pickerStyle causes only the picker part to be tappable.
 ///	Workaround: Setting .pickerStyle() to .menu (instead of .automatic, which is .menu on iOS) causes only the picker part to be tappable.
 ///
 ///	- FB12181540: Should be able to apply color and font to Picker.
-///	Workaround: None
+///	Workaround: None, see SnapTheme.ThemePickerListRow for a replacement.
 public struct PickerTapable<Label, SelectionValue, Content> : View where Label : View, SelectionValue : Hashable, Content : View {
 	
 	public init(style: any PickerStyle = .automatic, selection: Binding<SelectionValue>, content: @escaping () -> Content, label: @escaping () -> Label) {
