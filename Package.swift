3edce6d41a01084cc4d54717ec39841583c1f181
Simon Nickel <simonnickel@googlemail.com>
2024-10-26 11:45:58 +0200

    Add SnapFoundation

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
             name: "SnapCore",
             targets: ["SnapCore"]),
     ],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/simonnickel/snap-foundation.git", branch: "main"),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
