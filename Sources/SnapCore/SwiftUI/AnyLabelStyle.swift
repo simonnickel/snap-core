06c0cd0e4232d3043855dc2fc9111ab68f7044e4
Simon Nickel <simonnickel@googlemail.com>
2025-08-28 16:41:29 +0200

    Cleanup

--- Sources/SnapCore/SwiftUI/AnyLabelStyle.swift
+++ Sources/SnapCore/SwiftUI/AnyLabelStyle.swift
@@ -14,6 +14,7 @@ public struct AnyLabelStyle: LabelStyle {
 	}
 	
 	public func makeBody(configuration: Configuration) -> some View {
+        // TODO: Fix warning: Accessing Environment<Optional<CGFloat>>'s value outside of being installed on a View. This will always read the default value and will not update.
         AnyView(base.makeBody(configuration: configuration))
 	}
 	
