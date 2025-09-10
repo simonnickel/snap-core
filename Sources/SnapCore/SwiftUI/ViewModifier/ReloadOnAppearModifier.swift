71f1739662dcd225d01dfda4565fc4964bf3a43a
Simon Nickel <simonnickel@googlemail.com>
2025-09-10 15:57:26 +0200

    Add ReloadOnAppearModifier

---
+++ Sources/SnapCore/SwiftUI/ViewModifier/ReloadOnAppearModifier.swift
@@ -0,0 +1,21 @@
+//
+//  SNAP - https://github.com/simonnickel/snap
+//  Created by Simon Nickel
+//
+
+import SwiftUI
+
+/// Invalidates the view onAppear to trigger a rerender, used to work around SwiftUI bugs.
+public struct ReloadOnAppearModifier: ViewModifier {
+    
+    @State private var didAppear: Bool = false
+
+    public func body(content: Content) -> some View {
+        content
+            .id(didAppear)
+            .onAppear {
+                didAppear = true
+            }
+    }
+    
+}
