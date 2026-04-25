e74f918b2748d8c87dad444f265052b8f85563e6
Simon Nickel <simonnickel@googlemail.com>
2026-04-25 14:34:40 +0200

    Lower platform to iOS 17

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "snap-core",
 	platforms: [
-		.iOS(.v18), .macOS(.v15)
+		.iOS(.v17), .macOS(.v15)
 	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
